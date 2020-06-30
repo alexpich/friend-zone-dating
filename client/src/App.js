87f67320507eb9622d22f58edf55c75f70cabe20
Alex <pich.alex01@gmail.com>
2020-06-30 01:52:05 -0700

    Separated client and server

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+// Components
+import Nav from "./components/Nav";
+
+// Pages
+import Home from "./pages/home";
+import Friends from "./pages/friends";
+import Messages from "./pages/messages";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <Router>
+            <Nav />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/friends" component={Friends} />
+                <Route exact path="/messages" component={Messages} />
+                <Route exact path="/signin" component={Signin} />
+                <Route exact path="/signup" component={Signup} />
+              </Switch>
+            </div>
+          </Router>
+        </header>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+
+const SignupForm = () => (
+  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="red" textAlign="center">
+        <Image src="/logo.png" /> Create An Account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="E-mail address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button color="red" size="small">
+            Sign Up
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        Already have an account? <Link to="/signin">Sign In</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
+
+export default SignupForm;
